3887e6bbf04667dca888b4fbe9498757908b42af
Unai Herran <unai.herran@gmail.com>
2019-10-26 23:07:48 +0200

    calculo datos trafico

--- create_training_data.py
+++ create_training_data.py
@@ -130,7 +130,9 @@ def insert_en_train_1_db(fecha, cluster, num_coches=0, intensidad=0, ocupacion=0
                 # hay que hacer update
                 print('id = ', data[0][0])
 
-                sql = f"SELECT * FROM train_1"
+                sql = f"update train_1 set num_coches={num_coches}, intensidad={intensidad}, ocupacion={ocupacion}, " \
+                      f"carga={carga}, dia_semana={dia_semana} dia_mes={dia_mes}, festivo={festivo} " \
+                      f"WHERE id_train_1={data[0][0]}"
 
             else:
                 # hay que insertar

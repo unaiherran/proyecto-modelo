51746bc4db04f4193d67e1dceb434d232c29e9dc
Unai Herran <unai.herran@gmail.com>
2019-10-27 21:24:04 +0100

    dataframe para trafico

--- create_training_data2.py
+++ create_training_data2.py
@@ -59,6 +59,9 @@ def calcular_de_imagenes_camara(fecha):
                 df = df.sort_values(by=['cluster'])
                 df2 = df.groupby('cluster').mean()
                 df2 = df2.drop(['id_camara'], axis=1)
+                empty_df.to_csv('empty.csv')
+                df2.to_csv('df2.csv')
+
                 print(empty_df.head())
                 print(df2.head())
                 df3 = pd.merge(empty_df, df2, on='cluster', how='outer')

9ffddd3081fe08a5930320391826a9b5bc06ad77
Unai Herran <unai.herran@gmail.com>
2019-10-27 13:02:43 +0100

    calculo datso eventos

--- create_training_data.py
+++ create_training_data.py
@@ -42,6 +42,7 @@ def calcular_de_imagenes_camara(cluster, fecha):
                   f"AND str_to_date('{sig_fecha_str}', '%Y-%m-%d %H:%i')) and (clu.id_cluster = {cluster});"
 
             df = pd.read_sql(sql, con=connection)
+            print(df)
             num_coches = df.groupby('id_camara').mean().mean()['num_cars']
 
     return num_coches

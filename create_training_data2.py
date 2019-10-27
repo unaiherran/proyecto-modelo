5c5677d1dc37c063cbcba990a80dc62b67aac08a
Unai Herran <unai.herran@gmail.com>
2019-10-28 00:20:27 +0100

    dataframe para eventos

--- create_training_data2.py
+++ create_training_data2.py
@@ -218,7 +218,7 @@ def calcular_de_fecha(fecha):
 def insert_en_train_1_db(tb, df):
     if not LOCAL:
         if connection.is_connected():
-            df.to_sql(name='train_1', con=connection, if_exists='append')
+            df.to_sql(name='train_1', con=engine, if_exists='append')
 
 
 def calculo_parametros_un_train(fecha, tb='train_1'):

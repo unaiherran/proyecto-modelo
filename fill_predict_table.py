7074d750bfd0f8cae2501ab6a9613c8a8d7e4335
Unai Herran <unai.herran@gmail.com>
2019-11-20 19:36:02 +0100

    Fill predict

--- fill_predict_table.py
+++ fill_predict_table.py
@@ -85,7 +85,7 @@ def predict(num_cluster, in_table='train_1', out_table='predict'):
     df = df[['cluster', 'fecha', 'ocu_mean', 'ocu_pred']]
 
     #la prediccion se tiene que guardar en la siguiente fila.
-    df['predict'] = df['predict'].shift(1)
+    df['ocu_pred'] = df['ocu_pred'].shift(1)
 
     df = df.rename(columns={"ocu_mean": "ocu_real", "predict": "ocu_pred"})
 

2f5d16acc3d5a3cf5ef8d9da785faf3a07ab37f7
Unai Herran <unai.herran@gmail.com>
2019-11-26 20:59:23 +0100

    rellenar predicciones pasadas

--- sacar_medidas_reales_para_predict.py
+++ sacar_medidas_reales_para_predict.py
@@ -1,6 +1,9 @@
 from secret import *
 import mysql.connector
 
+from datetime import datetime
+from datetime import timedelta
+from time import sleep
 
 def main():
     connection = mysql.connector.connect(
@@ -11,7 +14,19 @@ def main():
         port=db_port)
 
     while True:
+        ahora = datetime.now()
+        if connection.is_connected():
+            cur = connection.cursor()
+            hora_maxima_consulta = ahora - timedelta(minutes=60)
+            sql = f"SELECT * FROM proyecto.predict where fecha < '{hora_maxima_consulta}' and ocu_medida is null limit 1;"
 
+            cur.execute(sql)
+
+            cluster = cur.fetchall()
+
+            print(cluster)
+
+            sleep(60)
 
 if __name__ == '__main__':
     main()
